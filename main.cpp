a31db1f3c60371548e6104b6ba8724769df0e0a7
Roman <rsheyko1997@gmail.com>
2019-02-18 19:49:56 +0300

    vertex shader compilation added

--- main.cpp
+++ main.cpp
@@ -19,6 +19,9 @@ void CheckSDLError(int line);
 void printVersion();
 void Run();
 void Cleanup();
+bool loadShader(const char *path, GLenum shader_type);
+GLchar* loadShaderAsString(const char *path);
+
 
 bool Init()
 {
@@ -52,6 +55,8 @@ bool Init()
     SetOpenGLAttributes();
 
     SDL_GL_SetSwapInterval(1);
+
+    loadShader("../basic.vert", GL_VERTEX_SHADER);
 
     return true;
 }
@@ -111,6 +116,159 @@ int main(int argc, char ** argv) {
     Run();
     return 0;
 }
+
+void Run()
+{
+    Uint32 start;
+    char running = 1;
+    float xrf = 0, yrf = 0, zrf = 0;
+    glm::vec4 position = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    glm::mat4 view = glm::lookAt(glm::vec3(0.0, 0.0, 5.0),
+            glm::vec3(0.0, 0.0, 0.0),
+            glm::vec3(0.0, 1.0, 0.0));
+
+    while(running != 0)
+    {
+        start = SDL_GetTicks();
+        SDL_Event e;
+        while(SDL_PollEvent(&e)) {
+            if(eventListener(e) == 0) {
+                running = 0;
+            }
+        }
+
+        xrf -= 0.5;
+        yrf -= 0.5;
+        zrf -= 0.5;
+
+        //drawCube(xrf, yrf, zrf);
+
+        glFlush();
+        SDL_GL_SwapWindow(window);
+
+        if(1000/FRAMES_PER_SECOND > SDL_GetTicks() - start)
+            SDL_Delay(1000/FRAMES_PER_SECOND - (SDL_GetTicks() - start));
+    }
+}
+
+GLchar* loadShaderAsString(const char *path)
+{
+    std::cout << "loading Shader..." << std::endl;
+    const int READ_CONST = 255;
+    bool memory_flag = false;
+    FILE *f = fopen(path, "r");
+    char *shaderCode = new char[READ_CONST];
+    int i = 0, symbol;
+
+    while((symbol = fgetc(f)) != EOF)
+    {
+        if(i < READ_CONST) shaderCode[i] = (char)symbol;
+        else memory_flag = true;
+        i++;
+    }
+
+    if(memory_flag)
+    {
+        delete [] shaderCode;
+        shaderCode = new char[i+1];
+        i = 0;
+        while((symbol = fgetc(f)) != EOF)
+        {
+            shaderCode[i] = (char)symbol;
+            i++;
+        }
+    }
+
+    shaderCode[i] = '\0';
+
+    return (GLchar*)shaderCode;
+}
+
+bool loadShader(const char *path, GLenum type)
+{
+    GLuint shader = glCreateShader(type);
+    if(shader == 0)
+    {
+        fprintf(stderr, "Error creating shader\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //Compile source
+    const GLchar *shaderCode = loadShaderAsString(path);
+    const GLchar *codeArray[] = {shaderCode};
+    glShaderSource(shader, 1, codeArray, NULL);
+
+    //Compile shader
+    glCompileShader(shader);
+
+    //check
+    GLint result;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+    if(result == GL_FALSE)
+    {
+        fprintf(stderr, "shader compilation failed!");
+
+        GLint logLen;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLen);
+
+        if(logLen > 0)
+        {
+            char *log = new char[logLen];
+
+            GLsizei written;
+            glGetShaderInfoLog(shader, logLen, &written, log);
+
+            fprintf(stderr, "Shader log:\n%s", log);
+            delete [] log;
+        }
+    }
+
+    return true;
+}
+
+void CheckSDLError(int line = -1)
+{
+    std::string error = SDL_GetError();
+
+    if (error != "")
+    {
+        std::cout << "SLD Error : " << error << std::endl;
+
+        if (line != -1)
+            std::cout << "\nLine : " << line << std::endl;
+
+        SDL_ClearError();
+    }
+}
+
+void PrintSDL_GL_Attributes()
+{
+    int value = 0;
+    SDL_GL_GetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, &value);
+    std::cout << "SDL_GL_CONTEXT_MAJOR_VERSION : " << value << std::endl;
+
+    SDL_GL_GetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, &value);
+    std::cout << "SDL_GL_CONTEXT_MINOR_VERSION: " << value << std::endl;
+}
+
+void printVersion()
+{
+    const GLubyte *renderer = glGetString(GL_RENDERER);
+    const GLubyte *vendor = glGetString(GL_VENDOR);
+    const GLubyte *version = glGetString(GL_VERSION);
+    const GLubyte *glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
+
+    GLint major, minor;
+    glGetIntegerv(GL_MAJOR_VERSION, &major);
+    glGetIntegerv(GL_MINOR_VERSION, &minor);
+
+    printf("GL Vendor           : %s\n", vendor);
+    printf("GL Renderer         : %s\n", renderer);
+    printf("GL Version (string) : %s\n", version);
+    printf("GL Version (integer): %d.%d\n", major, minor);
+    printf("GLSL Version        : %s\n", glslVersion);
+}
+
 /*
 void drawCube(float xrf, float yrf, float zrf){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -163,80 +321,3 @@ void drawCube(float xrf, float yrf, float zrf){
     glEnd();
 
 }*/
-
-void Run()
-{
-    Uint32 start;
-    char running = 1;
-    float xrf = 0, yrf = 0, zrf = 0;
-    glm::vec4 position = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
-    glm::mat4 view = glm::lookAt(glm::vec3(0.0, 0.0, 5.0),
-            glm::vec3(0.0, 0.0, 0.0),
-            glm::vec3(0.0, 1.0, 0.0));
-
-    while(running != 0)
-    {
-        start = SDL_GetTicks();
-        SDL_Event e;
-        while(SDL_PollEvent(&e)) {
-            if(eventListener(e) == 0) {
-                running = 0;
-            }
-        }
-
-        xrf -= 0.5;
-        yrf -= 0.5;
-        zrf -= 0.5;
-
-        //drawCube(xrf, yrf, zrf);
-
-        glFlush();
-        SDL_GL_SwapWindow(window);
-
-        if(1000/FRAMES_PER_SECOND > SDL_GetTicks() - start)
-            SDL_Delay(1000/FRAMES_PER_SECOND - (SDL_GetTicks() - start));
-    }
-}
-
-void CheckSDLError(int line = -1)
-{
-    std::string error = SDL_GetError();
-
-    if (error != "")
-    {
-        std::cout << "SLD Error : " << error << std::endl;
-
-        if (line != -1)
-            std::cout << "\nLine : " << line << std::endl;
-
-        SDL_ClearError();
-    }
-}
-
-void PrintSDL_GL_Attributes()
-{
-    int value = 0;
-    SDL_GL_GetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, &value);
-    std::cout << "SDL_GL_CONTEXT_MAJOR_VERSION : " << value << std::endl;
-
-    SDL_GL_GetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, &value);
-    std::cout << "SDL_GL_CONTEXT_MINOR_VERSION: " << value << std::endl;
-}
-
-void printVersion()
-{
-    const GLubyte *renderer = glGetString(GL_RENDERER);
-    const GLubyte *vendor = glGetString(GL_VENDOR);
-    const GLubyte *version = glGetString(GL_VERSION);
-    const GLubyte *glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
-
-    GLint major, minor;
-    glGetIntegerv(GL_MAJOR_VERSION, &major);
-    glGetIntegerv(GL_MINOR_VERSION, &minor);
-
-    printf("GL Vendor           : %s\n", vendor);
-    printf("GL Renderer         : %s\n", renderer);
-    printf("GL Version (string) : %s\n", version);
-    printf("GL Version (integer): %d.%d\n", major, minor);
-    printf("GLSL Version        : %s\n", glslVersion);
-}
